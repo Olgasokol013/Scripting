b9490cf4b7675367e839c465b161d6ccd1711366
Olgasokol013 <o850339@gmail.com>
2025-10-16 19:23:15 +0300

    Add files via upload

---
+++ Player.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    int health = 10;
+    public GameObject fireballPrefab;
+    public Transform attackPoint;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Instantiate(fireballPrefab,attackPoint.position, attackPoint.rotation);
+        }
+
+    }
+
+    public void TakeDamage(int damage)
+    {
+        
+        health -= damage;
+        print("Здоровье игрока: " + health);
+    }
+}
